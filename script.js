06d3d0d8831b67079ca7e3ac2206e444b75b872e
frxzerotwo-ctrl <frxzerotwo@gmail.com>
2026-09-26 20:53:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // --- CONFIGURATION ---
-const WORKER_URL = 'https://streamex-server.frxzerotwo.workers.dev';
+const API_KEY = 'af32459863d504e3a5d04f317e7f12e1';
 const BASE_URL = 'https://api.themoviedb.org/3';
+const WORKER_URL = ''; // سيبه فاضي
 const IMG_URL = 'https://image.tmdb.org/t/p/w342';
 const IMG_ORIG = 'https://image.tmdb.org/t/p/original';
 
